d4df39de7a3e0a515d2e90e3124679385b5d201d
Kennedy <kennedy.developper@gmail.com>
2025-05-28 23:24:10 +0300

    Build Flies

---
+++ Mobile/android/app/.cxx/Debug/2o4q505l/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-vision-camera/react-native-vision-cameraConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-vision-camera::VisionCamera)
+add_library(react-native-vision-camera::VisionCamera SHARED IMPORTED)
+set_target_properties(react-native-vision-camera::VisionCamera PROPERTIES
+    IMPORTED_LOCATION "/media/kennedy/3E509D70509D301F/ubuntuProject/doctimob/Doctimob/Mobile/node_modules/react-native-vision-camera/android/build/intermediates/cxx/Debug/26165y31/obj/armeabi-v7a/libVisionCamera.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/media/kennedy/3E509D70509D301F/ubuntuProject/doctimob/Doctimob/Mobile/node_modules/react-native-vision-camera/android/build/headers/visioncamera"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
