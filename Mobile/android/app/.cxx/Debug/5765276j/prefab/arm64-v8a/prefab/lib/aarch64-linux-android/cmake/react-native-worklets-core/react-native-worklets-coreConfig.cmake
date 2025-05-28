208e425bfe34fa549681b4b874b68012da222f00
Kennedy <kennedy.developper@gmail.com>
2025-05-28 22:16:56 +0300

    Remove .cxx

--- Mobile/android/app/.cxx/Debug/5765276j/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-worklets-core/react-native-worklets-coreConfig.cmake
+++
@@ -1,8 +0,0 @@
-if(NOT TARGET react-native-worklets-core::rnworklets)
-add_library(react-native-worklets-core::rnworklets INTERFACE IMPORTED)
-set_target_properties(react-native-worklets-core::rnworklets PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "/Users/etech/Documents/madaitlab/doctimob/doctimob/node_modules/react-native-worklets-core/android/build/headers/rnworklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
