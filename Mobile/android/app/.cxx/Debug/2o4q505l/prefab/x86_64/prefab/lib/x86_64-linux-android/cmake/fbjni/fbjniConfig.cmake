18c2a71ccc175ef189002150f0f1a646c883a8bb
rabenanf <66433618+rabenanf@users.noreply.github.com>
2025-05-29 10:07:07 +0300

    Delete Mobile/android/app/.cxx directory

--- Mobile/android/app/.cxx/Debug/2o4q505l/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/media/kennedy/Data/AndroidGradle/gradle/caches/8.12/transforms/a8b92c8222e2e1e6b43cad54fdde4c10/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/libs/android.x86_64/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/media/kennedy/Data/AndroidGradle/gradle/caches/8.12/transforms/a8b92c8222e2e1e6b43cad54fdde4c10/transformed/jetified-fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
