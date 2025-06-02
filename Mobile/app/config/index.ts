8469b605d966e695b349b162b8385a3b0f52f9d8
rabenanf <p.rabenandriamanitra@etechconsulting-mg.com>
2025-06-02 19:54:58 +0300

    config preprod

--- Mobile/app/config/index.ts
+++ Mobile/app/config/index.ts
@@ -7,6 +7,9 @@ const PAY_URL = 'http://51.77.247.28:44095/';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
 // SERVICE_ROLE_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogInNlcnZpY2Vfcm9sZSIsCiAgImlzcyI6ICJzdXBhYmFzZSIsCiAgImlhdCI6IDE3NDEwMzU2MDAsCiAgImV4cCI6IDE4OTg4MDIwMDAKfQ.UuPUnFx-6ow4GNnsFYLyy6KJo47-DzBbQkTQtfdBrO8'
 
+// const SUPABASE_URL = 'https://preprod-api.doctimob.com/';
+// const PAY_URL = 'https://preprod-api-data.doctimob.com/';
+// const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 export default {
     SUPABASE_URL, 
     SUPABASE_ANON_KEY,
