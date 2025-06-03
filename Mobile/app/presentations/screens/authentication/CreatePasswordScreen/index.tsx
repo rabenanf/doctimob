72bc6596230eb9d1b494f474f97094f02c3bffa0
Kennedy <kennedy.developper@gmail.com>
2025-06-03 13:55:02 +0300

    Fix Registration

--- Mobile/app/presentations/screens/authentication/CreatePasswordScreen/index.tsx
+++ Mobile/app/presentations/screens/authentication/CreatePasswordScreen/index.tsx
@@ -1,41 +1,38 @@
-import React, { JSX, useEffect, useState } from 'react';
-import { View, Text, Image } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import { useTranslation } from 'react-i18next';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { styles } from './styles';
-import { RootStackParamList } from '../../../../data/interface';
-import AppLayout from '../../../layout';
-import Logo from '../../../../resources/assets/images/logo.png';
-import { RoundedButton } from '../../../components/RoundedButton';
-import {PasswordWithIcon} from '../../../components/PasswordWithIcon';
-import {Theme} from '../../../../resources/themes'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import useUserStore from '../../../../services/redux/userStore';
+import React, { JSX, useEffect, useState } from "react";
+import { View, Text, Image } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { useTranslation } from "react-i18next";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { styles } from "./styles";
+import { RootStackParamList } from "../../../../data/interface";
+import AppLayout from "../../../layout";
+import Logo from "../../../../resources/assets/images/logo.png";
+import { RoundedButton } from "../../../components/RoundedButton";
+import { PasswordWithIcon } from "../../../components/PasswordWithIcon";
+import { Theme } from "../../../../resources/themes";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import useUserStore from "../../../../services/redux/userStore";
 import { UserService } from "../../../../services/application/user.sa";
-import { showToast } from '../../../../services/utils/toast';
-import { User } from '../../../../data/dto/User.type';
-import { CustomActivityIndicator } from '../../../components/CustomActivityIndicator';
+import { showToast } from "../../../../services/utils/toast";
+import { User } from "../../../../data/dto/User.type";
+import { CustomActivityIndicator } from "../../../components/CustomActivityIndicator";
 
-type Props = NativeStackScreenProps<RootStackParamList, 'CreatePassword'>;
+type Props = NativeStackScreenProps<RootStackParamList, "CreatePassword">;
 
-export const CreatePasswordScreen = ({navigation}: Props): JSX.Element => {
-    
-    const { t } = useTranslation();
+export const CreatePasswordScreen = ({ navigation }: Props): JSX.Element => {
+  const { t } = useTranslation();
 
-    const { user, updateUser } = useUserStore(); 
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const { createUserProfile } = UserService();
+  const { user, updateUser } = useUserStore();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<Partial<typeof form>>({});
+  const { createUserProfile } = UserService();
 
-
-    const form = {
-        password: password,
-        confirmPassword: confirmPassword
-    }
-        
-    const [errors, setErrors] = useState<Partial<typeof form>>({});
+  const form = {
+    password: password,
+    confirmPassword: confirmPassword,
+  };
 
     const createAccount = async () => {
         setLoading(true);
@@ -57,72 +54,78 @@ export const CreatePasswordScreen = ({navigation}: Props): JSX.Element => {
         setLoading(false);
     }
 
-    const validate = () => {
-        const newErrors: Partial<typeof form> = {};
-        if (password.length < 6) newErrors.password = t('CreatePassword.passwordError');
-        if (confirmPassword != password) newErrors.confirmPassword = t('CreatePassword.confirmPasswordError');
+  const validate = () => {
+    const newErrors: Partial<typeof form> = {};
+    if (password.length < 6)
+      newErrors.password = t("CreatePassword.passwordError");
+    if (confirmPassword != password)
+      newErrors.confirmPassword = t("CreatePassword.confirmPasswordError");
 
-        setErrors(newErrors);
-        return Object.keys(newErrors).length === 0;
-    };
-    
-    useEffect(() => {
-        console.log('-----utilisateur----', user);
-    }, []);
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
-    return (
-        <AppLayout isFullScreen={true} >
-            <LinearGradient 
-                style={styles.container} 
-                colors={[Theme.BACKGROUND_COLOR, 'white']}
-                start={{ x: 0.5, y: 0 }}
-                end={{ x: 0.5, y: 1 }}
-                locations={[0, 0.40]}
-                >
-                <View style={{ flex : 1}}>   
-                    <KeyboardAwareScrollView
-                        contentContainerStyle={{ flexGrow: 1 }}
-                        enableOnAndroid={true}
-                        keyboardShouldPersistTaps="handled"
-                    >
-                        <View style={styles.imageContainer}>
-                            <Image style={styles.image} resizeMode="contain" source={Logo}/> 
-                        </View>
-                        <View style={styles.formContainer}>
-                            <View style={styles.textContainer} >
-                                <Text style={styles.welcomeText}> {t('CreatePassword.title')} </Text>
-                                <Text style={styles.descriptionText}> {t('CreatePassword.description')} </Text>
-                            </View>
-                            <View style={styles.form}>
-                                <PasswordWithIcon
-                                    iconName="bag"
-                                    placeholder={t('CreatePassword.password')}
-                                    value={password}
-                                    onChangeText={setPassword}
-                                    error={errors.password}
-                                />
-                                <PasswordWithIcon
-                                    iconName="bag"
-                                    placeholder={t('CreatePassword.confirmPassword')}
-                                    value={confirmPassword}
-                                    onChangeText={setConfirmPassword}
-                                    error={errors.confirmPassword}
-                                />
+  useEffect(() => {
+    if (validate()) {
+      updateUser({ password: password });
+    }
+  }, [password, confirmPassword]);
 
-                            </View>
-                        </View>
-                    </KeyboardAwareScrollView> 
-                    <View style={styles.btnContainer}>
-                        <RoundedButton 
-                            isPrimary={true}
-                            onButtonPress={ () => { createAccount() }} 
-                            textBtn={t('CreatePassword.createAccount')}
-                        />
-                    </View>
-                </View>
-                { loading && <CustomActivityIndicator /> }
-            </LinearGradient>
-
-        </AppLayout>
-    )
-}
+  return (
+    <AppLayout isFullScreen={true}>
+      <LinearGradient
+        style={styles.container}
+        colors={[Theme.BACKGROUND_COLOR, "white"]}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        locations={[0, 0.4]}
+      >
+        <View style={{ flex: 1 }}>
+          <KeyboardAwareScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            enableOnAndroid={true}
+            keyboardShouldPersistTaps="handled"
+          >
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} resizeMode="contain" source={Logo} />
+            </View>
+            <View style={styles.formContainer}>
+              <View style={styles.textContainer}>
+                <Text style={styles.welcomeText}>
+                  {t("CreatePassword.title")}
+                </Text>
+                <Text style={styles.descriptionText}>
+                  {t("CreatePassword.description")}
+                </Text>
+              </View>
+              <View style={styles.form}>
+                <PasswordWithIcon
+                  iconName="bag"
+                  placeholder={t("CreatePassword.password")}
+                  value={password}
+                  onChangeText={setPassword}
+                  error={errors.password}
+                />
+                <PasswordWithIcon
+                  iconName="bag"
+                  placeholder={t("CreatePassword.confirmPassword")}
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  error={errors.confirmPassword}
+                />
+              </View>
+            </View>
+          </KeyboardAwareScrollView>
+          <View style={styles.btnContainer}>
+            <RoundedButton
+              isPrimary={true}
+              onButtonPress={createAccount}
+              textBtn={t("CreatePassword.createAccount")}
+            />
+          </View>
+        </View>
+        {loading && <CustomActivityIndicator />}
+      </LinearGradient>
+    </AppLayout>
+  );
+};
--- Mobile/app/services/application/user.sa.ts
+++ Mobile/app/services/application/user.sa.ts
@@ -3,139 +3,138 @@ import { supabase } from "../technical/supabase";
 import { LanguageService } from "./language.sa";
 
 export const UserService = () => {
+  const { getLanguageIdByCode } = LanguageService();
 
-    const { getLanguageIdByCode } = LanguageService();
+  return {
+    createUserProfile: async (user: Partial<User>) => {
+      let authenticationUser = null;
 
-    return {
-    
-        createUserProfile : async ( user : Partial<User> ) => {
+      console.log({ UserPayload: user });
 
-            let authenticationUser = null;
-            
-            let { data : userCreated, error } = await supabase.auth.signUp({
-                email: user.email!,
-                password: user.password!
-            })
+      let { data: userCreated, error } = await supabase.auth.signUp({
+        email: user.email!,
+        password: user.password!,
+      });
 
-            console.log('---------UserCreated---- : ', userCreated);
-            console.log('---------UserCreated---- : ', error);
+      console.log("---------UserCreated---- : ", userCreated);
+      console.log("---------UserCreated---- : ", error);
 
-            if (error) {
-                return {success : false, message : error.message};
-            }
+      if (error) {
+        return { success: false, message: error.message };
+      }
 
-            if (userCreated.user != null) {
-                authenticationUser = userCreated.user;
+      if (userCreated.user != null) {
+        authenticationUser = userCreated.user;
 
-                let userProfil : any = {
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    email: user.email,
-                    role: user.role,
-                    gender: user.gender,
-                    user_id: authenticationUser.id,
-                    phone: user.phone,
-                    birth_date: user.birth_date
-                };
-                
-                const { data : location, error: error1 } = await supabase
-                .from('localisations')
-                .insert({ lat: user.localisation?.lat, lon: user.localisation?.lon})
-                .select();
+        let userProfil: any = {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+          role: user.role,
+          gender: user.gender,
+          user_id: authenticationUser.id,
+          phone: user.phone,
+          birth_date: user.birth_date,
+        };
 
-                console.log('---------Location---- : ', location);
-            
-                let { data: language, error : error2 } = await supabase
-                    .from('languages')
-                    .select("*")
-                    .eq('code', 'en');
+        const { data: location, error: error1 } = await supabase
+          .from("localisations")
+          .insert({ lat: user.localisation?.lat, lon: user.localisation?.lon })
+          .select();
 
-                console.log('---------Language---- : ', language);
+        console.log("---------Location---- : ", location);
+        console.log("---------Add Location Error ---- : ", error1);
 
-                if (language != null && language.length > 0) {
-                    userProfil.language_id = language[0].id;
+        let { data: language, error: error2 } = await supabase
+          .from("languages")
+          .select("*")
+          .eq("code", "en");
 
-                } 
-                
-                if (location != null) {
-                    userProfil.localisation_id = location!.id;
-                }
+        console.log("---------Language---- : ", language);
+        console.log("---------Add User Language Error---- : ", error2);
 
-                console.log('---------ProfilUser---- : ', userProfil);
-
-                const { data, error } = await supabase
-                .from('user_profile')
-                .insert(userProfil).select();
-
-                console.log('---------User---- : ', data);
-                console.log('---------User---- : ', error);
-
-                if (error) {
-                    return {success : false, message : error.message};
-                }
-
-                
-
-                return {success: true, user: user}
-            }
-        },
-
-        updateUserProfile : async ( user : Partial<User>, id : string ) => {
-            const { data, error } = await supabase
-                .from('user_profile')
-                .update(user)
-                .eq('id', id)
-                .select();
-
-            if (error) {
-                return {success : false, message : error.message};
-            }
-
-            return {success: true}
-        },
-
-        updateLanguage : async ( id : string, code : string ) => {
-            const { data, error } = await supabase
-                .from('user_profile')
-                .update({ language_id: code })
-                .eq('id', id)
-                .select();
-
-            if (error) {
-                return {success : false, message : error.message};
-            }
-
-            return {success: true}
-        },
-
-        getUserProfile : async ( email : string ) => {
-            let { data: user, error } = await supabase
-                .from('user_profile')
-                .select("*")
-                .eq('email', email);
-
-            console.log('---------User---- : ', user);
-
-            if (error) {
-                return {success : false, message : error.message};
-            }
-
-            return {success: true, user: user}
-        },
-
-        getUserProfileByUserid : async ( userId : string ) => {
-            let { data: user, error } = await supabase
-                .from('user_profile')
-                .select(`*, speciality_doctor_id(name)`)
-                .eq('user_id', userId);
-
-            console.log('---------User---- : ', user);
-
-            if (error) {
-                return {success : false, message : error.message};
-            }
-
-            return {success: true, user: user![0] }
+        if (language != null && language.length > 0) {
+          userProfil.language_id = language[0].id;
         }
-    }
-}
+
+        if (location != null) {
+          userProfil.localisation_id = location!.id;
+        }
+
+        console.log("---------ProfilUser---- : ", userProfil);
+
+        const { data, error } = await supabase
+          .from("user_profile")
+          .insert(userProfil)
+          .select();
+
+        console.log("---------New User_profile Data---- : ", data);
+        console.log("---------Create User_profile Error---- : ", error);
+
+        if (error) {
+          return { success: false, message: error.message };
+        }
+
+        return { success: true, user: user };
+      }
+    },
+
+    updateUserProfile: async (user: Partial<User>, id: string) => {
+      const { data, error } = await supabase
+        .from("user_profile")
+        .update(user)
+        .eq("id", id)
+        .select();
+
+      if (error) {
+        return { success: false, message: error.message };
+      }
+
+      return { success: true };
+    },
+
+    updateLanguage: async (id: string, code: string) => {
+      const { data, error } = await supabase
+        .from("user_profile")
+        .update({ language_id: code })
+        .eq("id", id)
+        .select();
+
+      if (error) {
+        return { success: false, message: error.message };
+      }
+
+      return { success: true };
+    },
+
+    getUserProfile: async (email: string) => {
+      let { data: user, error } = await supabase
+        .from("user_profile")
+        .select("*")
+        .eq("email", email);
+
+      console.log("---------User---- : ", user);
+
+      if (error) {
+        return { success: false, message: error.message };
+      }
+
+      return { success: true, user: user };
+    },
+
+    getUserProfileByUserid: async (userId: string) => {
+      let { data: user, error } = await supabase
+        .from("user_profile")
+        .select(`*, speciality_doctor_id(name)`)
+        .eq("user_id", userId);
+
+      console.log("---------User---- : ", user);
+
+      if (error) {
+        return { success: false, message: error.message };
+      }
+
+      return { success: true, user: user![0] };
+    },
+  };
+};
